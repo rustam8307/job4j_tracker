a392694373b346a465d397301b0d7c00add197e8
macbookair <rustam8307>
2021-01-24 01:03:35 +0300

    2. Перегрузка конструктора [#414728]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,7 +2,23 @@ package ru.job4j.tracker;
 
 public class Item {
     private int id;
+
     private String name;
+
+    public Item(){
+
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id =id;
+    }
+
+
 
     public int getId() {
         return id;
